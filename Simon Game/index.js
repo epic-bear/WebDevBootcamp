e105a29959a3b1a1900c3e415c9e33ded4fde527
Yulian Belov <ybelo@softserveinc.com>
2023-11-02 15:09:35 +0200

    commit

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,3 +1,5 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomNumber = nextSequence();
 function nextSequence() {
-    return Math.ceil(Math.random() * 4);
+  return Math.ceil(Math.random() * 4);
 }
