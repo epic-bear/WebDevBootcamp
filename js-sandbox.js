69675fee2c7d3abd050404cabc24cb44bec13576
Yulian Belov <ybelo@softserveinc.com>
2023-11-03 17:48:40 +0200

    commit

--- js-sandbox.js
+++ js-sandbox.js
@@ -1,20 +1,11 @@
-function fibonacciGenerator (n) {
-    //Do NOT change any of the code above 👆
-        
-        //Write your code here:
-        if (n <= 1) {
-            return [0];
-        }
-        var result = [0,1];
+const fs =  require("fs");
 
-        for(i = 2; i < n; i++) {
-         result.push(result[i - 2] + result[i - 1]);
-        }
-        
-        return result;
-        //Return an array of fibonacci numbers starting from 0.
-        
-    //Do NOT change any of the code below 👇
-    }
+fs.writeFile("message.txt", "Hello", (err) => {
+  if (err) throw err;
+  console.log("file saved");
+});
 
-    console.log(fibonacciGenerator(2));
+fs.readFile("./message.txt","utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
