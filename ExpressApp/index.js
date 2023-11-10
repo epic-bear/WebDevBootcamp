0965bec47f49a89f9a55f1220d42067fcad9a85a
Yulian Belov <ybelo@softserveinc.com>
2023-11-10 15:54:33 +0200

    commit

--- ExpressApp/index.js
+++ ExpressApp/index.js
@@ -1,17 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const _dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("short"));
 
 app.get("/", (req, res) => {
-  res.send("main page");
+  res.sendFile(_dirname + "/public/index.html");
 });
 
-app.get("/info", (req, res) => {
-  res.send("info page");
+app.post("/submit", (req, res) => {
+  res.send(`<p>${req.body["name"]} ${req.body["age"]}`);
 });
 
 app.listen(port, () => {
