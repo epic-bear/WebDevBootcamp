0640ef26caef14001a5a2c84b48c7cc3941b0dde
Yulian Belov <ybelo@softserveinc.com>
2023-11-11 11:51:13 +0200

    commit

--- EJS/index.js
+++ EJS/index.js
@@ -1,1 +1,11 @@
 import express from 'express';
+import ejs from 'ejs';
+
+const app = express();
+const port = 4000;
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+})
