46a0cd626520af999cd6547b4711a6c7437210fa
Yulian Belov <ybelo@softserveinc.com>
2023-11-11 12:18:15 +0200

    commit

--- EJS/index.js
+++ EJS/index.js
@@ -3,22 +3,9 @@ import ejs from "ejs";
 
 const app = express();
 const port = 4000;
-var today;
-
-function dayOfTheWeek(req, res, next) {
-  if (new Date().getDay() < 6) {
-    today = "weekday, it's time to work hard!";
-  } else {
-    today = "weekend, it's time to have fun!";
-  }
-
-  next();
-}
-
-app.use(dayOfTheWeek);
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Hey! It's a ${today}`);
+  res.render("index.ejs", {dayType: "a weekday", advice: "it's time to work hard!"});
 });
 
 app.listen(port, () => {
